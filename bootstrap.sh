ce0957fb0c66853bde479fca021492abf1b63a1d
sterziev88 <66157650+sterziev88@users.noreply.github.com>
2022-02-10 15:34:43 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y amazon-linux-extras
-sudo yum install httpd -y
-sudo amazon-linux-extras enable php7.4
-sudo yum clean metadata
-sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd -y
-cd /tmp
-sudo wget http://wordpress.org/latest.tar.gz
-sudo tar -xzvf latest.tar.gz
-sudo cp -r wordpress/* /var/www/html/
-sudo chown -R apache:apache /var/www/html
-sudo systemctl restart httpd
+
+yum update -y
+yum install -y httpd
+echo '<h1>Hello HeleCloud</h1>' > /var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
